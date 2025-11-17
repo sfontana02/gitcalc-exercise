1752294275a2d149ae8ac9cb1c77bcb18b590948
Samuele Fontana <s.fontana9@studenti.unisa.it>
2025-11-17 15:46:41 +0100

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,11 @@
-print ("ciao cesare")
+def add(a, b):
+    """Aggiunge due numeri"""
+    return a + b
+def subtract(a, b):
+    """Sottrae il secondo numero dal primo"""
+    return a - b
+if __name__ == "_main_":
+    print("Benvenuto in GitCalc!")
+ #Test delle funzioni
+    print("Test Addizione (5+3):", add(5, 3))
+    print("Test Sottrazione (10-2):", subtract(10, 2))
